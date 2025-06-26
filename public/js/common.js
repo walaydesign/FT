b455fafb5b4fcacc03e53437d3f428fe86f1464e
youzhen <yl301114@gmail.com>
2025-06-26 16:30:17 +0800

    改popup, 換圖片格式

--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,20 @@
 $(".header_item").click(function(){
     let target = $(this).data("target");
     var top = $(target).offset().top - 78;
+    $(".detail").removeClass("active");
     $("html, body").animate({ scrollTop: top }, parseInt(300));
 })
 
 $(".warn_btn").click(function() {
     $(this).toggleClass("closed");
-    $(".warn_content").slideToggle(300);
+    $(this).parents(".warn").find(".warn_content").slideToggle(300);
 })
+
+$(".come_btn").click(function() {
+    let detail = $(this).data("detail");
+    $(detail).addClass("active");
+});
+
+$(".detail_close").click(function() {
+    $(this).parents(".detail").removeClass("active");
+});
